572ba6a0ebc072345e4b9414eef017b49ebd497c
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-09-07 11:47:17 +0200

    Wrap returned array of crowds in an object. Name the array field "crowds".

--- controllers/crowdController.js
+++ controllers/crowdController.js
@@ -39,7 +39,7 @@ module.exports = {
 
     getAll: function(req, res){
         Crowds.getAll(function(result){
-            res.json(result);
+            res.json({ crowds: result } );
         });
     },
 
