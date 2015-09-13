71dc293c722da38630f0bc53bc4a8d50cc69d343
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-09-13 21:55:48 +0200

    Refactoring. Make getUser-method used for the GET-request  use method getUserByUsername with the same code.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,44 +4,32 @@
 var Books = require('../models/book');
 var Crowds = require('../models/crowd.js');
 
+function getUser(req, res){
+    var username = req.params.username;
+    getUserByUsername(username, function(result){
+        res.json(result);
+    });
+}
+
+function getUserByUsername(username, callback){
+    var obj = {username: username}; // Add the username
+    Books.findRentedBy(username, function(result){
+        obj.booksRented = result !== 404 ? result:  [];
+        Books.findWithOwner(username, function(result){
+            obj.booksOwned = result !== 404 ? result : []; // The user's books
+            Crowds.getCrowdWithMember(username, function(result){
+                obj.crowds = [];
+                result.forEach(function(crowd, index){
+                    obj.crowds.push(crowd._id);
+                    if (index +1 === result.length) return callback(obj);
+                });
+            })
+        });
+    });
+}
+
+
 module.exports = {
-    getUser: function(req, res){
-        var username = req.params.username;
-        var obj = {username: username}; // Add the username
-        Books.findRentedBy(username, function(result){
-            obj.booksRented = result !== 404 ? result:  [];
-            Books.findWithOwner(username, function(result){
-                obj.booksOwned = result !== 404 ? result : []; // The user's books
-                Crowds.getAll(function(result){
-                    obj.crowds = [];
-                    for (var i = 0; i < result.length; i++){
-                        if (username in result[i]){
-                            obj.crowds.push(result[i]._id);
-                        }
-                    }
-                    return res.json(obj);  // send it
-                })
-            });
-        });
-    },
-
-
-    getUserByUsername: function(username, callback){
-        var obj = {username: username}; // Add the username
-        Books.findRentedBy(username, function(result){
-            obj.booksRented = result !== 404 ? result:  [];
-            Books.findWithOwner(username, function(result){
-                obj.booksOwned = result !== 404 ? result : []; // The user's books
-                Crowds.getAll(function(result){
-                    obj.crowds = [];
-                    for (var i = 0; i < result.length; i++){
-                        if (username in result[i]){
-                            obj.crowds.push(result[i]._id);
-                        }
-                    }
-                    return callback(obj);  // send it
-                })
-            });
-        });
-    }
+    getUser: getUser,
+    getUserByUsername: getUserByUsername
 };
