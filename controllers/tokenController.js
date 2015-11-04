2868ecb7fba754ff85d24de3b0932e7e797a6017
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-11-04 16:37:10 +0100

    Clean query and body for token-value in middleware.

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -11,18 +11,19 @@ var LEGAL_PATHS = ['/login', '/invite', '/forgotpassword', '/resetpassword' ];
 
 var validate = function (req, res, next) {
     // If request is not of method OPTIONS or is a request that's legal, check the token
-    if (req.method === 'OPTIONS' || isLegalPath(req.path) // Regular methods
-        || (req.method === 'POST' &&  (req.path === '/api/users' || req.path === '/api/users/') ) )  // Registration
-        return next();
-    else if (req.method !== 'OPTIONS' && ( req.body.token || req.query.token) ) {
+    if (req.method !== 'OPTIONS' && ( req.body.token || req.query.token) ) {
         var token = (req.body.token ? req.body.token : req.query.token); // Either body-token or query token
         return Tokens.isValid(token, function (tokenIsValid) {
             if (tokenIsValid) {
+                req.body = cleanObjForToken(req.body);
+                req.query = cleanObjForToken(req.query);
                 return next(); // Go on
             }
             return stndResponse.invalidToken(res); // Invalid token
         });
     }
+    else if (req.method === 'OPTIONS' || isLegalPath(req.path)|| isRegistrationRequest(req) ) // Registration and Regular methods
+        return next();
     return stndResponse.invalidToken(res); // Invalid token
 };
 
@@ -32,6 +33,16 @@ var isLegalPath =  function(path){
         if('/api' + item  === path || '/api' + item  + '/' ===  path) return true;
         if (index +1 === LEGAL_PATHS.length) return false;
     });
+};
+
+var cleanObjForToken = function (obj) {
+    newObj = obj;
+    delete newObj.token;
+    return newObj;
+};
+
+var isRegistrationRequest = function (req) {
+    return req.method === 'POST' &&  (req.path === '/api/users' || req.path === '/api/users/')
 };
 
 module.exports = {
